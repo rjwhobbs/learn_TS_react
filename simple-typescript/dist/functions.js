6c209b3d28b6c6b3c113435f751aa81ed5e20221
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-20 15:06:20 +0200

    Adding default vals to function args

--- simple-typescript/dist/functions.js
+++ simple-typescript/dist/functions.js
@@ -1,6 +1,6 @@
 "use strict";
 // Fucntions (Optional and default parms)
-function sum(a, b) {
-    return a + (b || 0); // Protecting if b was not given
+function sum(a, b, c = 42) {
+    return a + (b || 0) + c; // Protecting if b was not given
 }
 const sumTwo = (a, b) => a + b;
--- simple-typescript/src/functions.ts
+++ simple-typescript/src/functions.ts
@@ -1,7 +1,7 @@
 // Fucntions (Optional and default parms)
 
-function sum(a: number, b?: number): number {
-  return a + (b || 0); // Protecting if b was not given
+function sum(a: number, b?: number, c: number = 42): number {
+  return a + (b || 0) + c; // Protecting if b was not given
 }
 
 // sum(); Error with out arg optioanl key word '?'
