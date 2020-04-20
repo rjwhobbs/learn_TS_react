4d42eae1a76c22403b7c72ee8b21ba0d276ba4d8
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-20 12:39:53 +0200

    Using namespaces in TS

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mul_1 = __importDefault(require("./mul"));
-//Without fdefault
+// With default
+//import multiply,  {mulTwo} from './mul'; 
+//Without default
 //import {mul, mulTwo} from './mul'; 
+//With namespace
+const math = __importStar(require("./mul"));
 const a = 21;
 const b = 2;
-console.log(mul_1.default(a, b));
+console.log(math.default(a, b)); //Getting default export from namespace
+console.log(math.mulTwo(b));
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,8 +1,12 @@
-import multiply,  {mulTwo} from './mul'; 
-//Without fdefault
+// With default
+//import multiply,  {mulTwo} from './mul'; 
+//Without default
 //import {mul, mulTwo} from './mul'; 
+//With namespace
+import * as math from './mul';
 
 const a = 21;
 const b = 2;
 
-console.log(multiply(a, b));
+console.log(math.default(a, b)); //Getting default export from namespace
+console.log(math.mulTwo(b));
