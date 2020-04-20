d78d0e8ab2f7a5a4c3ef7a9ae90bee6f174f64f9
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-20 15:05:10 +0200

    Optional args in functions

--- simple-typescript/dist/functions.js
+++ simple-typescript/dist/functions.js
@@ -1,6 +1,6 @@
 "use strict";
 // Fucntions (Optional and default parms)
 function sum(a, b) {
-    return a + b;
+    return a + (b || 0); // Protecting if b was not given
 }
 const sumTwo = (a, b) => a + b;
--- simple-typescript/src/functions.ts
+++ simple-typescript/src/functions.ts
@@ -1,8 +1,10 @@
 // Fucntions (Optional and default parms)
 
-function sum(a: number, b: number): number {
-  return a + b;
+function sum(a: number, b?: number): number {
+  return a + (b || 0); // Protecting if b was not given
 }
+
+// sum(); Error with out arg optioanl key word '?'
 
 type MyFunc = (a: number, b: number) => number; // Creating out own function type
 
