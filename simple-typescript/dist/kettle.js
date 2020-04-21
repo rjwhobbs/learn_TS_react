2e0b16a5707eb6c8c0ca9f011b14efdf5ffe4f02
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-21 12:54:04 +0200

    Constructor in interface

--- simple-typescript/dist/kettle.js
+++ simple-typescript/dist/kettle.js
@@ -1,2 +1,1 @@
 "use strict";
-// We can only use interfaces to make public properties of the instance side of a class
--- simple-typescript/src/kettle.ts
+++ simple-typescript/src/kettle.ts
@@ -1,1 +1,5 @@
 // We can only use interfaces to make public properties of the instance side of a class
+interface Kettle {
+  volume: number;
+  new (volume: number): Kettle; // Here we discribe the constructor and ofcourse it's return type
+}
