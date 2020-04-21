1c3a33f763c3d0193b0e9973232bc8d2c7399a46
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-21 12:49:50 +0200

    We can only use interfaces to make public properties of the instance side of a class

---
+++ simple-typescript/dist/kettle.js
@@ -0,0 +1,2 @@
+"use strict";
+// We can only use interfaces to make public properties of the instance side of a class
---
+++ simple-typescript/src/kettle.ts
@@ -0,0 +1,1 @@
+// We can only use interfaces to make public properties of the instance side of a class
