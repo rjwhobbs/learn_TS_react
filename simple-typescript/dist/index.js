075901a25ac8dae90f64cedb578641eb7624f0a2
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-21 14:15:09 +0200

    Extending interface and then making arrays of those types

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Generics
 // Generic functions
 function genericFunc(x) {
@@ -14,3 +15,12 @@ class GenClass {
         return this.props;
     }
 }
+// So we can make interfaces for arrays aswell?
+const chocoCakes = [
+    { expiryDate: new Date() }
+];
+const vanillaCakes = [
+    { expiryDate: new Date() }
+];
+const a = [{ x: 7 }];
+console.log(a[0].x);
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,3 +1,5 @@
+import { chocolate } from "color-name";
+
 // Generics
 // Generic functions
 function genericFunc<T>(x: T): T {
@@ -25,3 +27,32 @@ class GenClass<P>{
     return this.props;
   }
 }
+
+interface Expirable {
+  expiryDate: Date;
+}
+
+// I think the difference between implements and extends is that with extend you actually 
+// inherit where as with implements you just implement.
+interface ChocCake extends Expirable {}
+
+interface VanillaCake extends Expirable {}
+
+// So we can make interfaces for arrays aswell?
+const chocoCakes: ChocCake[] = [
+  {expiryDate: new Date()}
+];
+
+const vanillaCakes: VanillaCake[] = [
+  {expiryDate: new Date()}
+];
+
+// A small test of the code above
+interface TestOne {
+  x: number;
+}
+
+interface TestTwo extends TestOne {}
+
+const a: TestTwo[] = [{x: 7}];
+console.log(a[0].x);
