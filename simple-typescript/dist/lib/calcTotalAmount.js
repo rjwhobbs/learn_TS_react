6f682e810f58a4d6b95b3e64fac375e13d069f7b
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-21 12:42:20 +0200

    So implements is for interfaces?

--- simple-typescript/dist/lib/calcTotalAmount.js
+++ simple-typescript/dist/lib/calcTotalAmount.js
@@ -5,7 +5,7 @@ function calcTotalAmount(order) {
     let total = order.calcTotal();
     const discount = total * 0.1; // In js we can say .1 , but this gives my eyes cancer
     total -= discount;
-    const tax = total * 0.15;
+    const tax = total * 0.2;
     total += tax;
     return total;
 }
--- simple-typescript/src/lib/calcTotalAmount.ts
+++ simple-typescript/src/lib/calcTotalAmount.ts
@@ -7,7 +7,7 @@ export function calcTotalAmount(order: InterfaceOrder) {
   let total = order.calcTotal();
   const discount = total * 0.1; // In js we can say .1 , but this gives my eyes cancer
   total -= discount;
-  const tax = total * 0.15;
+  const tax = total * 0.2;
   total += tax;
   return total;
 }
