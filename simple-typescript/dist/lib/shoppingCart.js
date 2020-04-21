d2a3dc6e4de4ab9302e9d85e0cfdc7b75392c5a4
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-21 12:31:39 +0200

    'Contracting' modules

--- simple-typescript/dist/lib/shoppingCart.js
+++ simple-typescript/dist/lib/shoppingCart.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class ShoppingCart {
+    calcTotal() {
+        return 100;
+    }
 }
 exports.ShoppingCart = ShoppingCart;
--- simple-typescript/src/lib/shoppingCart.ts
+++ simple-typescript/src/lib/shoppingCart.ts
@@ -1,5 +1,8 @@
+// These two modules don't know about each other, they are not tied to each other, they jus tshare the same interface
 import { InterfaceOrder } from "./calcTotalAmount";
 
 export class ShoppingCart implements InterfaceOrder {
-
+  calcTotal() {
+    return 100;
+  }
 }
