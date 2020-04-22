492cf8864a2328909e0363343ce9d33141a44f0a
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-22 10:16:31 +0200

    Assertions

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -8,3 +8,15 @@ function func(arg) {
         //code
     }
 }
+function isDog(pet) {
+    return pet.bark() !== undefined; //Making an assertion on the object
+}
+function callPet(pet) {
+    pet.walk();
+    if (isDog(pet)) {
+        pet.bark();
+    }
+    else {
+        pet.meow();
+    }
+}
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -6,3 +6,27 @@ function func(arg: string | number | boolean) {
     //code
   }
 }
+
+//Type gaurd with interfaces
+interface Cat {
+  walk(): void;
+  meow(): void;
+}
+
+interface Dog {
+  walk(): void;
+  bark(): void;
+}
+
+function isDog(pet: Dog | Cat): pet is Dog  {
+  return (<Dog>pet).bark() !== undefined //Making an assertion on the object
+}
+
+function callPet(pet: Dog | Cat) {
+  pet.walk()
+  if (isDog(pet)) {
+    pet.bark();
+  } else {
+    pet.meow();
+  }
+}
