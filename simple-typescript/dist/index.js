e31024b0265cbbc0d763d1667f26e4f0ad6ded44
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-22 11:49:03 +0200

    So ts is able to make res the correct type shape thanks to generics but we can still input primitive types in to this function

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -19,10 +19,11 @@ let dd = cc;
 dd.a = 99;
 console.log(cc); // Sp object spread doesn't make a deep copy
 // Function to combine objects
+// So ts is able to make res the correct type shape thanks to generics but we can still input primitive types in to this function
 function com(objA, objB) {
     return Object.assign(Object.assign({}, objA), objB);
 }
 const oA = { a: 1 };
 const oB = { b: 2 };
-const res = com(oA, oB); // So we need to avoid 'any' as much as we can, this seems to be the point of ts
+const res = com('', oB); // So we need to avoid 'any' as much as we can, this seems to be the point of ts
 console.log(res);
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,4 +1,4 @@
-//Intersection types
+//Intersection types 16.
 interface IA {
   a: number;
 }
@@ -32,12 +32,13 @@ dd.a = 99;
 console.log(cc); // Sp object spread doesn't make a deep copy
 
 // Function to combine objects
-function com(objA, objB) {
+// So ts is able to make res the correct type shape thanks to generics but we can still input primitive types in to this function
+function com<ObjA, ObjB>(objA: ObjA, objB: ObjB) {
   return {...objA, ...objB};
 }
 
 const oA = {a: 1};
 const oB = {b: 2};
-const res = com(oA, oB); // So we need to avoid 'any' as much as we can, this seems to be the point of ts
+const res = com('', oB); // So we need to avoid 'any' as much as we can, this seems to be the point of ts
 
 console.log(res);
