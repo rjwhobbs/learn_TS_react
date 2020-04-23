11c74cf81b51f210dda5e81fc3f9876308e27a93
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-23 13:39:13 +0200

    Cool instancetype and mix ins just cooked my brain

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const prop_types_1 = require("prop-types");
 const updateA = (id, a) => {
     if (a.x) {
         return a.x + id;
@@ -20,3 +21,47 @@ const map = {
         y: 4
     }
 };
+function thing(test) {
+    console.log(typeof test);
+}
+// thing('cheese'); // gives error but works?
+thing('red');
+// thing(undefined); // With strict null check set to true this produces an error
+// ReturnType<T> extracting the type of retrun from a function
+function fn() {
+    return { a: prop_types_1.number, b: prop_types_1.string };
+}
+function Deletable(Base) {
+    return class extends Base {
+        constructor() {
+            super(...arguments);
+            this.deleted = false; // false to silence warning
+        }
+        delete() { }
+        ;
+    };
+}
+class Car {
+    constructor(name) {
+        this.name = name;
+    }
+    ;
+}
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+    ;
+}
+const DeletableCar = Deletable(Car);
+const DeletableUser = Deletable(User);
+//Errors cause vscode wants these props to be defined
+class Profile {
+    constructor() {
+        this.user = new DeletableUser('Mic');
+        this.car = new DeletableCar('Train');
+    }
+}
+const profile = new Profile();
+console.log(profile);
+// ThisType<T>
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,4 +1,4 @@
-import { string } from "prop-types";
+import { string, number } from "prop-types";
 
 // Utility types
 // Partial<T> makes all props of an interface optional
@@ -44,3 +44,69 @@ type test = Pick<AA, 'x'>;
 type Nums = 1 | 2 | 3 | 4 | 5 | 6 ;
 type Even = 2 | 4 | 6;
 type Odd = Exclude<Nums, Even>;
+
+//Extract<T, U> 
+
+type prime = 1 | 2 | 3 | 5 | 7;
+type pTest = Extract<Nums, prime>; // 7 won't get extracted
+
+// NonNullable<T> only on union types however strict null checks must be true
+
+interface BB {
+  id: number;
+  color?: 'red' | 'blue';
+}
+
+function thing(test: NonNullable<BB['color']>) { // some new js syntax
+  console.log(typeof test);
+}
+
+// thing('cheese'); // gives error but works?
+thing('red');
+// thing(undefined); // With strict null check set to true this produces an error
+
+// ReturnType<T> extracting the type of retrun from a function
+
+function fn() {
+  return {a: number, b: string}
+}
+
+type FnReturn = ReturnType<typeof fn>;
+
+// InstanceType<T>, the type of the static side of a class 20:00 chap 20
+// Mix in patterns?
+
+type Constructable<ClassInstance> = new (...args: any[]) => ClassInstance; 
+
+function Deletable<BassClass extends Constructable<{}>>(Base: BassClass) {
+  return class extends Base {
+      deleted: boolean = false; // false to silence warning
+      delete() {};
+  }
+}
+
+class Car {
+  constructor(public name: string) {};
+}
+
+class User {
+  constructor(public name: string) {};
+}
+
+const DeletableCar = Deletable(Car);
+const DeletableUser = Deletable(User);
+
+type DUInstance = InstanceType<typeof DeletableUser>;
+type DCInstance = InstanceType<typeof DeletableUser>;
+
+//Errors cause vscode wants these props to be defined
+class Profile { 
+  user:  DUInstance = new DeletableUser('Mic');
+  car: DCInstance = new DeletableCar('Train');
+}
+
+const profile = new Profile();
+
+console.log(profile);
+
+// ThisType<T>
