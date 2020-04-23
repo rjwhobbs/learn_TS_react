152bd845bd9510f4bb5adf8bb178f9e57edf51a2
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-23 11:32:05 +0200

    Partial utility

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
 // Utility types
+// Partial<T> makes all props of an interface optional
+const updateA = (id, a) => {
+    if (a.x) {
+        return a.x + id;
+    }
+    return 0;
+};
+console.log(updateA(1, { x: 23 }));
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,1 +1,17 @@
 // Utility types
+// Partial<T> makes all props of an interface optional
+
+// So I'm getting conflicts from other ts files on interface 'A' hence 'AA'
+interface AA {
+  x: number;
+  y: number;
+}
+
+const updateA = (id: number, a: Partial<AA>) => {
+  if (a.x) {
+    return a.x + id;
+  }
+  return 0;
+}
+
+console.log(updateA(1,{x: 23}));
