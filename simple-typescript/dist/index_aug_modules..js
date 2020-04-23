d6de24f2c7315ba452b9d00b505a3b6b1f93e354
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-23 11:23:20 +0200

    cleaning

---
+++ simple-typescript/dist/index_aug_modules..js
@@ -0,0 +1,24 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Augmenting a module
+// Chap 19
+const React = __importStar(require("react"));
+const server_1 = require("react-dom/server");
+// We can augment Component because react exports it
+React.Component.prototype.howzit = function () {
+    return "Howzit";
+};
+console.log(React.Component.prototype.howzit());
+class MyComp extends React.Component {
+    render() {
+        return (React.createElement("div", null, this.howzit()));
+    }
+}
+console.log(server_1.renderToString(React.createElement(MyComp, null)));
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,2 +1,1 @@
-"use strict";
 // Utility types
