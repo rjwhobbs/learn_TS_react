b723cce567283fe76b76767dc7835364c4598b11
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-23 10:13:04 +0200

    Handling modules that don't have types, refer to chap 18.

--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -1,12 +1,4 @@
-//Type-Alias
-type MyAlias = string | string[] | null;
-type Alias = {a: number} & {b: number};
-type potato<T> = T[];
-
-type Cheese = {
-  a: number;
-  b: number;
-}
-
-// We use can use an alias to describe the shape of an object however it is advised to ratheuse interfaces for that
-// and use aliases for complex types as in the above first 3
+// installing typed packages
+// npm i -D @types/name
+// Chap 18.
+// Declaring modules
