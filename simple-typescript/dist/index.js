a857d15756568c7bd15a3775b08ef37f9e395277
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-24 13:10:05 +0200

    Assertion

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -35,7 +35,7 @@ else if (typeof data === 'object') {
 // 1. TS forces you to determine the type of the unknown value before you can work with it:
 let anyValue = 1;
 let unknownValue = 1;
-anyValue.helloWorld(); // No error.
+//anyValue.helloWorld(); // No error, but runtime error
 // unknownValue.helloWorld(); // you'll get a TS error.
 // 2. You cannot assign unknown to anything but itself or any, while you can assign any to anything:
 /*
@@ -48,3 +48,17 @@ str = anyValue; // No error.
 str = unknownValue; // you'll get a TS error.
 */
 //These constraints make sure that you verify the unknown value before working with it, which is important to reduce the possibility of a runtime error.
+//Type assertion (you can't simply assert that a value can have any type you want)
+// You can't assert a value to be of type which doesn't overlap with the actual type of the value:
+/*
+    let b = '1' as number; // Error, you can't say that a string is a number.
+     
+    interface User {
+      name: string;
+      email: string;
+    }
+     
+    function saveUser(user: User) {}
+     
+    saveUser(<User>{ helloWorld: '' }); // Error, this object doesn't have the required properties of the User interface.
+*/ 
--- simple-typescript/src/index.ts
+++ simple-typescript/src/index.ts
@@ -47,7 +47,7 @@ if (typeof data === 'string') {
 let anyValue: any = 1;
 let unknownValue: unknown = 1;
  
-anyValue.helloWorld(); // No error.
+//anyValue.helloWorld(); // No error, but runtime error
 // unknownValue.helloWorld(); // you'll get a TS error.
 
 // 2. You cannot assign unknown to anything but itself or any, while you can assign any to anything:
@@ -63,3 +63,19 @@ str = unknownValue; // you'll get a TS error.
 
 //These constraints make sure that you verify the unknown value before working with it, which is important to reduce the possibility of a runtime error.
 
+//Type assertion (you can't simply assert that a value can have any type you want)
+
+// You can't assert a value to be of type which doesn't overlap with the actual type of the value:
+
+/*
+    let b = '1' as number; // Error, you can't say that a string is a number.
+     
+    interface User {
+      name: string;
+      email: string;
+    }
+     
+    function saveUser(user: User) {}
+     
+    saveUser(<User>{ helloWorld: '' }); // Error, this object doesn't have the required properties of the User interface.
+*/
