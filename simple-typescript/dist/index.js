1ed97809cdd7236e1406225e1d36b2ec33e9ae9a
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-04-24 13:05:34 +0200

    Using unkown

--- simple-typescript/dist/index.js
+++ simple-typescript/dist/index.js
@@ -19,3 +19,32 @@ type AddType = (a: number, b: number) => number
 let XX = 1; // is equivalent to let XX: number = 1;
 function log(message) { }
 log(1); // is equivalent to log<number>(1)
+// TypeScript includes the type called unknown. We can use this type to describe a value which we don't know. For example, a value returned from a network call:
+const getSomeDataFromSomeAPICall = () => { }; // silence
+let data = getSomeDataFromSomeAPICall();
+if (typeof data === 'string') {
+    // data is a string.
+}
+else if (Array.isArray(data)) {
+    // data is an array.
+}
+else if (typeof data === 'object') {
+    // data is an object.
+}
+// Now you'd ask: "Why don't use any in this case?". The differences are:
+// 1. TS forces you to determine the type of the unknown value before you can work with it:
+let anyValue = 1;
+let unknownValue = 1;
+anyValue.helloWorld(); // No error.
+// unknownValue.helloWorld(); // you'll get a TS error.
+// 2. You cannot assign unknown to anything but itself or any, while you can assign any to anything:
+/*
+let anyValue: any = 1;
+let unknownValue: unknown = 1;
+ 
+let str: string;
+ 
+str = anyValue; // No error.
+str = unknownValue; // you'll get a TS error.
+*/
+//These constraints make sure that you verify the unknown value before working with it, which is important to reduce the possibility of a runtime error.
